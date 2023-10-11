7c2c45d978601c87b438156e193b068e74225ca5
kwlayton1 <kwlayton1@yahoo.com>
2023-10-11 02:10:11 -0600

    latest chart files requre debug

--- charts/chart.js
+++ charts/chart.js
@@ -1,10 +1,161 @@
-// Initialize the chart with an empty object
-let coChart = {};
+let selectedPollutant = "CO"; // Initialize with CO
+
+// Create separate chart variables for each pollutant
+let coChart, no2Chart, so2Chart, pm10Chart, o3Chart;
+
+// Create separate canvas elements for each pollutant
+const coCanvas = document.getElementById("coChart");
+const no2Canvas = document.getElementById("no2Chart");
+const so2Canvas = document.getElementById("so2Chart");
+const pm10Canvas = document.getElementById("pm10Chart");
+const o3Canvas = document.getElementById("o3Chart");
+
+// Initialize all chart instances for different pollutants
+coChart = new Chart(coCanvas, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Year",
+        },
+      },
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "CO Levels",
+        },
+      },
+    },
+  },
+});
+
+no2Chart = new Chart(no2Canvas, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Year",
+        },
+      },
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "NO2 Levels",
+        },
+      },
+    },
+  },
+});
+
+so2Chart = new Chart(so2Canvas, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Year",
+        },
+      },
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "SO2 Levels",
+        },
+      },
+    },
+  },
+});
+
+pm10Chart = new Chart(pm10Canvas, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Year",
+        },
+      },
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "PM10 Levels",
+        },
+      },
+    },
+  },
+});
+
+o3Chart = new Chart(o3Canvas, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Year",
+        },
+      },
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "O3 Levels",
+        },
+      },
+    },
+  },
+});
 
 // Fetch the JSON data using D3.js
 d3.json("data/histUpdate.json").then(function (data) {
-  // Extract unique cities from the data
-  const uniqueCities = [...new Set(data.map(entry => entry.City))];
+  console.log(data);
+  // Extract all unique cities from the data
+  const uniqueCities = [...new Set(data.map(entry => entry.City)];
 
   // Populate the dropdown options with unique cities
   const citySelect = document.getElementById("citySelect");
@@ -14,64 +165,32 @@ d3.json("data/histUpdate.json").then(function (data) {
     option.textContent = city;
     citySelect.appendChild(option);
   });
-
-  function updateChart(selectedCity) {
-    // Filter the data for the selected city and "CO" pollutant
-    const filteredData = data.filter(entry => entry.City === selectedCity && entry.Pollutant === "CO");
-
-    // Check if data exists for the selected city and pollutant
-    if (filteredData.length === 0) {
-      console.log("No data found for the selected city and pollutant.");
-      return;
-    }
-
-    // Extract years and values for CO data within the range 2000 to 2022
+  
+  // Add an event listener to the dropdown to update the charts when a city is selected
+  citySelect.addEventListener("change", function () {
+    const selectedCity = citySelect.value;
+    updateChart(selectedCity);
+    // Extract years and values for the selected pollutant data within the range 2000 to 2022
     const years = Object.keys(filteredData[0]).filter(key => !isNaN(key) && key >= "2000" && key <= "2022");
     const values = years.map(year => parseFloat(filteredData[0][year]));
-
-    // Destroy the previous chart instance if it exists
-    if (coChart.destroy) {
-      coChart.destroy();
+  
+    // Destroy the previous chart instance if it exists and update the chart for the selected pollutant
+    switch (selectedPollutant) {
+      case "CO":
+        if (coChart.destroy) {
+          coChart.destroy();
+        }
     }
-
-    // Update the chart with the new data
-    const ctx = document.getElementById("coChart").getContext("2d");
-    coChart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: years,
-        datasets: [{
-          label: `CO Levels in ${selectedCity}`,
-          data: values,
-          borderColor: "blue",
-          borderWidth: 2,
-          fill: false,
-        }],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          x: {
-            beginAtZero: true,
-            title: {
-              display: true,
-              text: "Year",
-            },
-          },
-          y: {
-            beginAtZero: true,
-            title: {
-              display: true,
-              text: "CO Levels",
-            },
-          },
-        },
-      },
-    });
-  }
-
-  // Initialize the chart with the first city in the dropdown
-  const initialSelectedCity = uniqueCities[0];
-  updateChart(initialSelectedCity);
+  });
 });
+
+// Add an event listener to the button to update the charts when the button is clicked
+const updateChartButton = document.getElementById("updateChartButton");
+updateChartButton.addEventListener("click", function () {
+  const selectedCity = citySelect.value;
+  updateChart(selectedCity);
+});
+
+// Initialize the chart with the first city in the dropdown
+const initialSelectedCity = uniqueCities[0];
+updateChart(initialSelectedCity);
