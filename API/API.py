c9c618229664365b193116dcca3186df7787d300
smoot888 <charliesmoot@icloud.com>
2023-10-11 20:34:24 -0600

    updated api

--- API/API.py
+++ API/API.py
@@ -8,15 +8,9 @@ import numpy as np
 from flask import Flask, jsonify
 #Setup dabases
 # Create a reference to the file.
-City = Path("../data/city.sqlite")
-County = Path("../data/county.sqlite")
 Historical = Path("../data/historical2.sqlite")
-engine_City = create_engine(f"sqlite:///{City}")
-engine_County = create_engine(f"sqlite:///{County}")
 engine_Historical = create_engine(f"sqlite:///{Historical}")
-#Base = automap_base()
-#Base.prepare(autoload_with=engine)
-#city_ORM = Base.classes.keys()
+
 #Create Flask API
 app = Flask(__name__)
 
@@ -26,24 +20,8 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/city<br/>"
-        f"/api/v1.0/county<br/>"
         f"/api/v1.0/historical<br/>"
     )
-@app.route("/api/v1.0/city")
-def city():
-    session = Session(engine_City)
-    results = session.execute(text('SELECT * FROM city')).fetchall()
-    headers = session.execute(text('PRAGMA table_info(city)')).fetchall()
-    json_data = []
-    for index,row in enumerate(results):
-        row_dict = {}
-        for i in range(len(headers)):
-            row_dict[headers[i][1]] = results[index][i]
-        json_data.append(row_dict)
-        session.close()
-    
-    return json_data
 
 @app.route("/api/v1.0/historical")
 def historical():
@@ -59,22 +37,6 @@ def historical():
         session.close()
     
     return json_data
-
-@app.route("/api/v1.0/county")
-def county():
-    session = Session(engine_County)
-    results = session.execute(text('SELECT * FROM county')).fetchall()
-    headers = session.execute(text('PRAGMA table_info(county)')).fetchall()
-    json_data = []
-    for index,row in enumerate(results):
-        row_dict = {}
-        for i in range(len(headers)):
-            row_dict[headers[i][1]]= results[index][i]
-        json_data.append(row_dict)
-        session.close()
-    
-    return json_data
-
 
 if __name__ == '__main__':
     app.run(debug=True)
